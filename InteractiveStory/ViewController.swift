e87a3d9a68264d94a181e31b5d97e64f7d191d5d
gtokman <gtokman@fullsail.edu>
2016-05-09 19:35:54 -0400

    Added UI elements to view, add elements to subview with constraints, initialized the UI elements

--- InteractiveStory/ViewController.swift
+++ InteractiveStory/ViewController.swift
@@ -9,19 +9,23 @@
 import UIKit
 
 class ViewController: UIViewController {
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+		let story = Page(story: .TouchDown)
+		story.firstChoice = (title: "Title", page: Page(story: .Droid))
+	}
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        let story = Page(story: .TouchDown)
-        story.firstChoice = (title: "Title", page: Page(story: .Droid))
-    }
+	// MARK: - Navigtation
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
+	override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+		if segue.identifier == "StartAdventure" {
+			if let pageController = segue.destinationViewController as? PageController {
+				pageController.page = Adventure.story
+			}
+		}
+	}
+    
 
 }
 
